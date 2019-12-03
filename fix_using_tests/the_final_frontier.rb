07665bf0388492208d0d9b5ce3eb624236a15391
Noah Scala <tobi8912@gmail.com>
2019-12-03 01:19:32 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,4 @@
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -12,7 +13,8 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  greeted_crew = crew.map {|crew_member| "Hello #{crew_member}."}
+  greeted_crew
 end
 
 def engage
